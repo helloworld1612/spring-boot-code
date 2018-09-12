92f0fa0aaffdd58cb9cc2f71499373d8df074da3
yan <yan@yandeMacBook-Pro.local>
2018-09-12 15:11:41 +0800

    Initial commit

---
+++ src/main/java/com/yd/springbootcode/SpringBootCodeApplication.java
@@ -0,0 +1,12 @@
+package com.yd.springbootcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCodeApplication.class, args);
+    }
+}
